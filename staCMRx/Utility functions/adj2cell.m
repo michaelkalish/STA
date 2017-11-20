786882ce8c1ae4b19821789e7ff449ca477751c3
Luke <doctorlog@gmail.com>
2017-11-21 09:21:44 +1030

    Move the readme pdf to the root, delete matlab code from the non-matlab directory.

--- staCMRx/Utility functions/adj2cell.m
+++
@@ -1,7 +0,0 @@
-function E = adj2cell(adj)
-% converts adjacency matrix to cell array
-[row, col] = find(adj==1);
-E=cell(1,numel(row));
-for i=1:numel(row)
-    E{i} = [row(i) col(i)];
-end
--- staCMRx/Utility functions/num2adj.m
+++
@@ -1,9 +0,0 @@
-function adj = num2adj(x)
-% function adj = num2adj(x)
-% converts a vector of numbers into an adjacency matrix
-if isrow(x)
-    x = x'; % make sure x is a column vector
-end
-u = repmat(x,1,numel(x));
-adj = u - u'; % difference matrix
-adj(adj > 0) = 0; adj(adj < 0) = 1; % convert to adjacency
