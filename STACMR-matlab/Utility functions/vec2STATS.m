678e1a62b551f2d7ea550872b6b004e7b2646384
johncdunn55 <35297481+johncdunn55@users.noreply.github.com>
2018-04-26 02:05:06 +0000

    Add files via upload
    Optional changes added

--- STACMR-matlab/Utility functions/vec2STATS.m
+++ STACMR-matlab/Utility functions/vec2STATS.m
@@ -1,18 +1,22 @@
-function y = vec2STATS (x)
-% function y = vec2STATS (x)
+function y = vec2STATS (x,s,n)
+% function y = vec2STATS (x,s,n)
 % converts data in vector format to pseudo-staSTATS format (i.e. structured
 % cell array)
-% x = ncond*nvar matrix
-ncond = size(x,1);
-nvar = size(x,2);
+% x = ncond*nvar matrix of means
+% s = ncond*nvar matrix of standard deviations
+% n = ncond*nvar matrix of n's
+%
+ncond = size(x,1); nvar = size(x,2);
+if nargin < 3, n = ones(ncond,nvar); end
+if nargin < 2, s = ones(ncond,nvar); end
 y = cell(1,nvar);
 for ivar=1:nvar
     y{ivar}.means = x(:,ivar);
-    y{ivar}.n = ones(ncond);
-    y{ivar}.cov = eye(ncond);
-    y{ivar}.regov = eye(ncond);
-    y{ivar}.shrinkage=0;
-    y{ivar}.weights = eye(ncond);
-    y{ivar}.lm = eye(ncond);
+    y{ivar}.n = diag(n(:,nvar)); % put in matrix format
+    y{ivar}.cov = diag(s(:,ivar).^2); % convert to variances
+    y{ivar}.regcov = y{ivar}.cov; % assumes no shrinkage
+    y{ivar}.shrinkage=0; % like I said
+    y{ivar}.weights = y{ivar}.n.*y{ivar}.regcov^-1; % now we have weights
+    y{ivar}.lm = y{ivar}.regcov; % not used but may as well park something there
     y{ivar}.shrink=0;
 end
